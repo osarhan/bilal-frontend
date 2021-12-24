280973c758754f889ba3c162fbfd95fc3eada37b
Omar Sarhan <omars@zillowgroup.com>
2021-12-24 00:18:08 -0800

    minor fixes

--- src/components/Speakers.vue
+++ src/components/Speakers.vue
@@ -1,6 +1,18 @@
 <template>
   <q-page class="flex flex-center">
     <p>Speakers</p>
+    <q-knob
+        show-value
+        class="text-white q-ma-md"
+        v-model="volume"
+        size="70px"
+        :thickness="0.2"
+        color="blue"
+        center-color="grey-8"
+        track-color="transparent"
+    >
+      <q-icon name="volume_up"/>
+    </q-knob>
   </q-page>
 </template>
 
@@ -9,6 +21,11 @@
 
 <script>
 export default {
-  name: 'Speakers'
+  name: 'Speakers',
+  data() {
+    return {
+      'volume': 25
+    }
+  },
 }
 </script>
